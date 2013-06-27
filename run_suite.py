c74bae112c830799d88d5977f55a0fe332d7954c
Yaroslav Halchenko <debian@onerussian.com>
2013-06-27 14:43:07 -0400

    make use of just added run_order='multires'

--- run_suite.py
+++ run_suite.py
@@ -17,7 +17,8 @@ from suite import *
 def run_process():
     runner = BenchmarkRunner(benchmarks, REPO_PATH, REPO_URL,
                              BUILD, DB_PATH, TMP_DIR, PREPARE,
-                             run_option='eod', start_date=START_DATE,
+                             run_option='eod', run_order='multires',
+                             start_date=START_DATE,
                              module_dependencies=dependencies)
     runner.run()
 
