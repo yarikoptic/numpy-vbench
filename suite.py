8b1ba9a589d64901b1c98b88397010d58a624183
Yaroslav Halchenko <debian@onerussian.com>
2013-07-01 17:59:14 -0400

    provide cpuinfo for those demanding details

--- suite.py
+++ suite.py
@@ -61,6 +61,28 @@ Collect on a following machine:
   - Debian wheezy, amd-64 build (chroot on Debian with 3.2.0-4-amd64 kernel)
   - Python 2.7.3 64-bit
 
+cpuinfo::
+
+    vendor_id       : AuthenticAMD
+    cpu family      : 15
+    model           : 5
+    model name      : AMD Opteron(tm) Processor 246
+    stepping        : 8
+    microcode       : 0x46
+    cpu MHz         : 1994.032
+    cache size      : 1024 KB
+    fpu             : yes
+    fpu_exception   : yes
+    cpuid level     : 1
+    wp              : yes
+    flags           : fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush mmx fxsr sse sse2 syscall nx mmxext lm 3dnowext 3dnow rep_good nopl
+    bogomips        : 3988.06
+    TLB size        : 1024 4K pages
+    clflush size    : 64
+    cache_alignment : 64
+    address sizes   : 40 bits physical, 48 bits virtual
+    power management: ts ttp
+
 """
 dependencies = ['numpy_vb_common.py']
 
