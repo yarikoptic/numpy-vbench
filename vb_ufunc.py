a5acb7932f32c2cbcd0728ca1014d98bd5ccc5bc
Julian Taylor <jtaylor.debian@googlemail.com>
2013-07-11 19:04:26 +0200

    increase array size for vectorization benchmarks
    1000 is too small to see much change, it is dominated by numpy overhead.

--- vb_ufunc.py
+++ vb_ufunc.py
@@ -45,30 +45,30 @@ for ufunc in ufuncs:
 # some interesting selective tests
 vb_ufunc_custom = [
     Benchmark('numpy.nonzero(d)',
-              common_setup + 'd = numpy.ones(1000, dtype=numpy.bool)',
+              common_setup + 'd = numpy.ones(20000, dtype=numpy.bool)',
                 name='numpy.nonzero'),
-    Benchmark('~d', common_setup + 'd = numpy.ones(1000, dtype=numpy.bool)',
+    Benchmark('~d', common_setup + 'd = numpy.ones(20000, dtype=numpy.bool)',
               name='numpy.not_bool'),
     Benchmark('d & d',
-              common_setup + 'd = numpy.ones(1000, dtype=numpy.bool)',
+              common_setup + 'd = numpy.ones(20000, dtype=numpy.bool)',
               name='numpy.and_bool'),
     Benchmark('d | d',
-              common_setup + 'd = numpy.ones(1000, dtype=numpy.bool)',
+              common_setup + 'd = numpy.ones(20000, dtype=numpy.bool)',
               name='numpy.or_bool')]
 
 for type in ('numpy.float32', 'numpy.float64'):
     vb_ufunc_custom += [
         Benchmark('numpy.add(d, 1)',
-                  common_setup + 'd = numpy.ones(1000, dtype=%s)' % type,
+                  common_setup + 'd = numpy.ones(20000, dtype=%s)' % type,
                   name='numpy.add_scalar2_' + type),
         Benchmark('numpy.divide(d, 1)',
-                  common_setup + 'd = numpy.ones(1000, dtype=%s)' % type,
+                  common_setup + 'd = numpy.ones(20000, dtype=%s)' % type,
                   name='numpy.divide_scalar2_' + type),
         Benchmark('numpy.divide(d, 1, out=d)',
-                  common_setup + 'd = numpy.ones(1000, dtype=%s)' % type,
+                  common_setup + 'd = numpy.ones(20000, dtype=%s)' % type,
                   name='numpy.divide_scalar2_inplace_' + type),
         Benchmark('d < 1',
-                  common_setup + 'd = numpy.ones(1000, dtype=%s)' % type,
+                  common_setup + 'd = numpy.ones(20000, dtype=%s)' % type,
                   name='numpy.less_than_scalar2_' + type)]
 
 vb_ufunc_scalar = [
