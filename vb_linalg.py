d5c472845fe7a0fabe4eb3f14d887d418f1d8f5a
Yaroslav Halchenko <debian@onerussian.com>
2013-07-17 10:32:18 -0400

    ENH: enriched linalg benchmarks collection

--- vb_linalg.py
+++ vb_linalg.py
@@ -11,13 +11,28 @@ from numpy_vb_common import *
 setup = common_setup
 
 vb_linalg = []
-# SVDs
-cmd = 'numpy.linalg.svd(a)'
-for t in squares_:
-    # check that dtype is supported at all
-    try:
-        _ = numpy.linalg.svd(squares_[t][:2, :2])
-    except TypeError:
-        continue
-    vb_linalg.append(Benchmark(cmd, setup + "a=squares_[%r]" % t,
-                               name="%s_%s" % (cmd, t)))
+
+for op in ('svd', 'inv', 'det', 'norm'):
+    cmd = 'numpy.linalg.%s(a)' % op
+    func = getattr(numpy.linalg, op)
+    for t in squares_:
+        if op == 'cholesky':
+            # we need a positive definite
+            astr = "numpy.dot(squares_[%r], squares_[%r].T)" % (t, t)
+            atest = numpy.dot(squares_[t], squares_[t].T)
+        else:
+            astr = "squares_[%r]" % t
+            atest = squares_[t]
+
+        # check that dtype is supported at all
+        try:
+            _ = func(atest[:2, :2])
+        except TypeError:
+            continue
+        vb_linalg.append(Benchmark(cmd, setup + "a=%s" % astr,
+                                   name="%s_%s" % (cmd, t)))
+
+# add basic lstsq test
+vb_linalg.append(Benchmark('numpy.linalg.lstsq(a, b)',
+                           setup + "a=squares_['float64']; b=indexes_rand[:100].astype(numpy.float64)",
+                           name="numpy.linalg.lstsq(a, b)_float64"))
