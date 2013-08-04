6ed265129ea1beb85db3cbfaa87d8fa952050ac7
Yaroslav Halchenko <debian@onerussian.com>
2013-08-04 13:36:55 -0400

    generate reports for multiple branches

--- generate_rst_files.py
+++ generate_rst_files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from suite import benchmarks, DB_PATH, RST_BASE, DESCRIPTION, REPO_BROWSE
+from suite import benchmarks, DB_PATH, RST_BASE, DESCRIPTION, REPO_BROWSE, BRANCHES
 from vbench.reports import generate_rst_files, generate_rst_analysis
 
 generate_rst_analysis(
@@ -11,6 +11,7 @@ generate_rst_analysis(
 generate_rst_files(benchmarks,
                    dbpath=DB_PATH,
                    outpath=RST_BASE,
+                   branches=BRANCHES,
                    description=DESCRIPTION + """
 
 .. include:: analysis.rst
