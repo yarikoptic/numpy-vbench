8ec5930447addc536c4b927a689032c0bf54bf0a
Yaroslav Halchenko <debian@onerussian.com>
2013-08-21 14:15:37 -0400

    vb_indexing -- operate on smaller arrays to get benchmarks to more reasonable durations

--- vb_indexing.py
+++ vb_indexing.py
@@ -12,19 +12,19 @@ setup = common_setup
 
 vb_indexing = []
 vb_indexing_separate = []
-for l in ['indexes', 'indexes_rand']:
+for l in ['indexes_', 'indexes_rand_']:
     for sel in (l, ':,%s' % l, 'np.ix_(%s, %s)' % (l, l)):
         for op in ['', '=1']:
             act = "a[%s]%s" % (sel, op)
             name = act
 
             vb_indexing.append(
-                Benchmark('for a in squares.itervalues(): %s' % (act,), setup, name=name))
+                Benchmark('for a in squares_.itervalues(): %s' % (act,), setup, name=name))
 
             """
             for t in TYPES1:
                 vb_indexing_separate.append(
-                    Benchmark(act, setup + "\na = squares[%r]" % t,
+                    Benchmark(act, setup + "\na = squares_[%r]" % t,
                               name='%s_%s' % (name, t)))
             """
 
