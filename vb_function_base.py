9d4f5917b3bba8d20379e5d57053db4d11418805
Julian Taylor <jtaylor.debian@googlemail.com>
2013-11-09 00:23:35 +0100

    ENH: more benchmarks
    * multidimensional constructors (?stack)
    * small medians and explicit small simple reduction
    * count_nonzero
    * copyto
    * bincount
    * broadcasting
    * typed min/max
    * where/select

--- vb_function_base.py
+++ vb_function_base.py
@@ -12,7 +12,12 @@ vb_median = [Benchmark("np.median(e)", setup=setup, name="median_even"),
              Benchmark("np.median(e, overwrite_input=True)", setup=setup,
                        name="median_even_inplace"),
              Benchmark("np.median(o, overwrite_input=True)", setup=setup,
-                       name="median_odd_inplace")]
+                       name="median_odd_inplace"),
+             Benchmark("np.median(e[:500], overwrite_input=True)", setup=setup,
+                       name="median_even_small"),
+             Benchmark("np.median(o[:500], overwrite_input=True)", setup=setup,
+                       name="median_odd_small"),
+             ]
 
 vb_perc = [Benchmark("np.percentile(e, [25, 75])", setup=setup, name="quartile"),
            Benchmark("np.percentile(e, [25, 35, 55, 65, 75])", setup=setup,
@@ -21,3 +26,33 @@ vb_perc = [Benchmark("np.percentile(e, [25, 75])", setup=setup, name="quartile")
 vb_sort = [Benchmark("np.sort(e)", setup=setup, name="sort"),
            Benchmark("e.sort()", setup=setup, name="sort_inplace"),
            Benchmark("e.argsort()", setup=setup, name="argsort")]
+
+vb_bincount = Benchmark("np.bincount(d)",
+                        setup="d = np.arange(80000, dtype=np.intp)",
+                        name="bincount")
+vb_bincountw = Benchmark("np.bincount(d, weights=e)",
+                        setup="d = np.arange(80000, dtype=np.intp); "\
+                        "e = d.astype(np.float64)",
+                        name="bincount_weights")
+
+vb_where = [Benchmark("np.where(cond)",
+                      setup="d = np.arange(20000); cond = d > 5000",
+                      name="where_1"),
+            Benchmark("np.where(cond, d, e)",
+                      setup="d = np.arange(20000); "\
+                      "e = d.copy(); cond = d > 5000",
+                      name="where_2"),
+            Benchmark("np.where(cond, d, 0)",
+                      setup="d = np.arange(20000); cond = d > 5000",
+                      name="where_2_broadcast"),
+           ]
+
+vb_select = [Benchmark("np.select(cond, [d, e])",
+                       setup="d = np.arange(20000); e = d.copy();"\
+                       "cond = [d > 4, d < 2]",
+                       name="select"),
+             Benchmark("np.select(cond, [d, e] * 10)",
+                       setup="d = np.arange(20000); e = d.copy();"\
+                       "cond = [d > 4, d < 2] * 10",
+                       name="select_larger"),
+            ]
